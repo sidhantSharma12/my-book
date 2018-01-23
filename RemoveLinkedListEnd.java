926dbacb20aa979cfeeb3e4c3670a1d83578b1cb
Sidhant Sharma <sidhant0312@gmail.com>
2018-01-22 23:57:35 -0500

    Update RemoveLinkedListEnd.java

--- RemoveLinkedListEnd.java
+++ RemoveLinkedListEnd.java
@@ -1,28 +1,18 @@
 //Given a linked list, remove the nth node from the end of list and return its head.
 
 
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        List<ListNode> list= new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            list.add(temp);
-            temp=temp.next;
-        }
-        if(n==1){
-            if(list.size()==1){
-                return null;
-            }
-            ListNode secondLast = list.get(list.size()-2);
-            secondLast.next=null;
-        }
-        else if(n == list.size()){
-            head=head.next;
-        }
-        else{
-            ListNode listNode = list.get(list.size()-n-1);
-            listNode.next = listNode.next.next;
-        }    
-        return head;
+ ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = dummy;
+    ListNode second = dummy;
+    // Advances first pointer so that the gap between first and second is n nodes apart
+    for (int i = 1; i <= n + 1; i++) {
+        first = first.next;
     }
-}
+    // Move first to the end, maintaining the gap
+    while (first != null) {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
